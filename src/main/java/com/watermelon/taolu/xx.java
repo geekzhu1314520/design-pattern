499a1aa56a9059df23e42666f0ef5f55885eecef
zhujiashuai <zhujiashuai@meituan.com>
2020-07-05 14:12:27 +0800

    first commit

---
+++ src/main/java/com/watermelon/taolu/xx.java
@@ -0,0 +1,11 @@
+package com.watermelon.taolu;
+
+/**
+ * @author: zhujiashuai
+ * @date: 2020/7/5
+ */
+public class xx {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
